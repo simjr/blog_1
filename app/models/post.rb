cd5b54ea48f0dcbdaa128111cab14f149f146061
Simon Rojas <simonjrojas@gmail.com>
2017-09-17 10:35:35 -0400

    Iteration 3 - Modificated Models, added relations has_many, belongs_to...

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+    has_many :comments, dependent: :destroy
+    #el dependent destroy se coloca para decir que los comentarios que dependan del post
+    #serán destruidos al borrar un post
+    belongs_to :user
 end
