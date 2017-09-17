e15bc4287fa239a74c24ab113f4b8876eb57aabc
Simon Rojas <simonjrojas@gmail.com>
2017-09-17 11:01:00 -0400

    Added Validations - Iteration 4

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ApplicationRecord
     #el dependent destroy se coloca para decir que los comentarios que dependan del post
     #serán destruidos al borrar un post
     belongs_to :user
+    validates_presence_of :title
+    validates_presence_of :body
 end
